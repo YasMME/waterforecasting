c147845d9944045565155ba5bbadd93d1db3ae82
Yasmeen <yemampoor@ccs.neu.edu>
2018-05-23 13:05:38 -0700

    dictionaries of dataframes per borough

--- waterforecasting.py
+++ waterforecasting.py
@@ -5,12 +5,16 @@ import matplotlib.pyplot as plt #to show, plt.show()
 import tensorflow as tf
 from dateutil.parser import parse
 import datetime
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
 
 #read in csv file
 data = pd.read_csv('Water_Consumption_And_Cost__2013_-_2017_.csv',
         header=0)
 
 #drop unused columns
+### REMOVE IF I KEEP USING THE MONTHLY_DATA ###
 data = data.drop(['Development Name', 'Account Name', 'Location', 
     'Funding Source', 'Meter AMR', 'Meter Scope', 'TDS #', 'EDP', 
     'RC Code', 'AMP #', 'Vendor Name', 'UMIS BILL ID','Meter Number',
@@ -20,7 +24,6 @@ data = data.drop(['Development Name', 'Account Name', 'Location',
 #filter based on days of service
 data = data.loc[(data['# days'] > 26) & (data['# days'] < 34)]
 
-#combine all data points for a borough in a month
 
 #get list of boroughs
 boroughs = pd.Series(data['Borough'].values).unique()
@@ -28,22 +31,25 @@ boroughs = pd.Series(data['Borough'].values).unique()
 revenue_months = pd.Series(data['Revenue Month'].values).unique()
 
 
-monthly_data = pd.DataFrame(columns=('Month', 'Borough',
-    'Consumption (HCF)'))
+
+### Create a dictionary of DataFrames, keyed by Borough ###
+frames_by_borough = {}
 for b in boroughs:
     temp_borough = data.loc[(data['Borough'] == b)]
+    monthly_data = pd.DataFrame(columns=('Month', 'Consumption (HCF)'))
     for m in revenue_months:
         #strip extra date info
         d = parse(m, fuzzy=True)
         d = d.date() 
-
-        #sum consumption at all locations in a borough
+        #sum consumption at all locations in the borough
         monthly = temp_borough.loc[(data['Revenue Month'] == m)]
         total = monthly['Consumption (HCF)'].sum()
-
         monthly_data = monthly_data.append({'Month':d,
-                'Borough':b, 'Consumption (HCF)':total},
+                'Consumption (HCF)':total},
                 ignore_index=True)
+        monthly_data = monthly_data.sort_values(by='Month',
+                ascending=True)
+    frames_by_borough[b] = monthly_data
 
-#put data in chronological order
-monthly_data = monthly_data.sort_values(by='Month', ascending=True)
+#frames_by_borough['QUEENS']['Consumption (HCF)'] gets consumption for
+# queens data
