10f356002a7479bc5e827ba5e285757a08198bb5
Yasmeen <yemampoor@ccs.neu.edu>
2018-05-23 11:04:15 -0700

    monthly_data--one point per month per borough

--- waterforecasting.py
+++ waterforecasting.py
@@ -3,6 +3,8 @@ import pandas as pd
 import math
 import matplotlib.pyplot as plt #to show, plt.show()
 import tensorflow as tf
+from dateutil.parser import parse
+import datetime
 
 #read in csv file
 data = pd.read_csv('Water_Consumption_And_Cost__2013_-_2017_.csv',
@@ -25,11 +27,23 @@ boroughs = pd.Series(data['Borough'].values).unique()
 #get revenue months
 revenue_months = pd.Series(data['Revenue Month'].values).unique()
 
-queens_data = pd.DataFrame()
-for m in revenue_months:
-    #get all data points for a month, sum
-print(queens_data)
 
+monthly_data = pd.DataFrame(columns=('Month', 'Borough',
+    'Consumption (HCF)'))
+for b in boroughs:
+    temp_borough = data.loc[(data['Borough'] == b)]
+    for m in revenue_months:
+        #strip extra date info
+        d = parse(m, fuzzy=True)
+        d = d.date() 
 
+        #sum consumption at all locations in a borough
+        monthly = temp_borough.loc[(data['Revenue Month'] == m)]
+        total = monthly['Consumption (HCF)'].sum()
 
+        monthly_data = monthly_data.append({'Month':d,
+                'Borough':b, 'Consumption (HCF)':total},
+                ignore_index=True)
 
+#put data in chronological order
+monthly_data = monthly_data.sort_values(by='Month', ascending=True)
