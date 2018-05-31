c9c792276ead0f4f799edb57295c4a7fe2080ee1
Yasmeen <yemampoor@ccs.neu.edu>
2018-05-31 13:19:10 -0700

    timeseries_to_supervised

--- waterforecasting.py
+++ waterforecasting.py
@@ -10,10 +10,19 @@ from keras.models import Sequential
 from data_in import data_in
 
 data = data_in()
-print(data)
+
+borough1 = data['BRONX']
 
 ### Attribution:
 ###https://machinelearningmastery.com/time-series-forecasting-long-short-term-memory-network-python/
 def timeseries_to_supervised(data, lag=1):
-    #empty
-    return null
+    df = pd.DataFrame(data)
+    cols= [df.shift(i) for i in range(1, lag+1)]
+    cols.append(df)
+    df = pd.concat(cols, axis=1)
+    df.fillna(0, inplace=True)
+    df.columns = ["Month", "Consumption (HCF)", "Next Month",
+    "Consumption (HCF)"]
+    return df
+
+timeseries_to_supervised(borough1)
